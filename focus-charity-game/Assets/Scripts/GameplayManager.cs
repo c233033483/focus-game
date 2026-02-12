959a27d7ddb5ad6a2237892e95aa8451afedd911
c233033483 <saraheithne@gmail.com>
2026-02-12 12:56:44 +0000

    Cinemachine added

--- focus-charity-game/Assets/Scripts/GameplayManager.cs
+++ focus-charity-game/Assets/Scripts/GameplayManager.cs
@@ -5,6 +5,17 @@ public class GameplayManager : MonoBehaviour
     private int dayIndex; // 1-5
     private int customersToday; // 1-however many customers in the game
     
-    
+    public CustomerSO[] customers;
     private CustomerSO currentCustomer;
+
+
+    private void DayStart()
+    {
+        dayIndex++;
+    }
+
+    private void NextCustomer()
+    {
+        //currentCustomer = customers[??]; have a list for each day and which customers appear at what order, then can call from the index of the corresponding day
+    }
 }
